4520a57cd1ce2621de21a0b0223dc12c0f705918
FelipeTorquato <felipectorquato1@gmail.com>
2025-04-24 08:29:22 -0300

    fix Main

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int escolha = 0;
+        int escolha;
         double valor;
         String par;
-        ConsultaConversao consulta = new ConsultaConversao();
-        Conversao conversao = null;
-        while (escolha != 7) {
+        do {
             System.out.println("*************************************************");
             System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]\n\n");
             System.out.print("""
@@ -18,33 +16,55 @@ public class Main {
                     3) Dólar =>> Real brasileiro
                     4) Real brasileiro =›> Dólar
                     5) Dólar =>> Peso colombiano
-                    6) Peso colonbiano =>> Dólar
+                    6) Peso colombiano =>> Dólar
                     7) Sair
                     Escolha uma opção válida:
                     """);
 
             escolha = scanner.nextInt();
 
-            System.out.print("Digite o valor que deseja converter: ");
-            valor = scanner.nextDouble();
+            if (escolha != 7) {
+                System.out.print("Digite o valor que deseja converter: ");
+                valor = scanner.nextDouble();
 
-            switch (escolha) {
-                case 1:
-                    par = "USD/ARS/";
-                    conversao = consulta.getConversao(par, valor);
-                    exibeResultado(conversao.getBase(), conversao.getAlvo(), valor, conversao.getResultado());
-                    break;
-                case 2:
-                    par = "ARS/USD/";
-                    conversao = consulta.getConversao(par, valor);
-                    exibeResultado(conversao.getBase(), conversao.getAlvo(), valor, conversao.getResultado());
-                    break;
+                switch (escolha) {
+                    case 1:
+                        par = "USD/ARS/";
+                        chamaConversao(par, valor);
+                        break;
+                    case 2:
+                        par = "ARS/USD/";
+                        chamaConversao(par, valor);
+                        break;
+                    case 3:
+                        par = "USD/BRL/";
+                        chamaConversao(par, valor);
+                        break;
+                    case 4:
+                        par = "BRL/USD/";
+                        chamaConversao(par, valor);
+                        break;
+                    case 5:
+                        par = "USD/COP/";
+                        chamaConversao(par, valor);
+                        break;
+                    case 6:
+                        par = "COP/USD/";
+                        chamaConversao(par, valor);
+                        break;
+                }
             }
-        }
+        } while (escolha != 7);
     }
 
     private static void exibeResultado(String base, String alvo, double valorInicial, double valorFinal) {
-        System.out.println("Valor " + valorInicial + " [" + base + "] corresponde ao valor final de " +
-                "=>>> " + valorFinal + "[" + alvo + "]");
+        System.out.printf("Valor %f [%s] corresponde ao valor final de =>>> %f [%s]\n", valorInicial, base, valorFinal, alvo);
+    }
+
+    private static void chamaConversao(String par, Double valor) {
+        ConsultaConversao consulta = new ConsultaConversao();
+        Conversao conversao;
+        conversao = consulta.getConversao(par, valor);
+        exibeResultado(conversao.getBase(), conversao.getAlvo(), valor, conversao.getResultado());
     }
 }
